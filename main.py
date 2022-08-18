4eceea6e4e2d7a755083dd4088bc7400f6bd5d51
Matthew Sloss <msloss@calpoly.edu>
2022-08-17 17:13:03 -0700

    Began solver logic setup

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import member_maker
 import steel_solver
+import load_maker
 
 
 def main():
     aisc_df = pd.read_excel('__aisc-database-v15__.xlsx', sheet_name='Database v15.0')
-    member = member_maker.MemberMaker(aisc_df, 'W12X279', 10)
-    calc_tension = steel_solver.TensionMember(member, 25)
-    print(calc_tension.check_slenderness())
-    print(calc_tension.check_gross_yielding())
+    while True:
+        try:
+            calculation = str(input("Tension/Compression/Flexural/Combined?"))
+            if calculation == 'Tension' or calculation == 'tension':
+                tension()
+            else: raise ValueError
+            break
+        except ValueError:
+            print("Invalid or Unsupported input")
+    # member_df = member_maker.MemberMaker(aisc_df, 'W12X279', 10)
+    # calc_tension = steel_solver.TensionMember(member_df, 25)
+    # print(calc_tension.check_slenderness())
+    # print(calc_tension.check_gross_yielding())
+
+def tension():
+    length = input("How long is the member being designed?")
 
 
 if __name__ == "__main__":
