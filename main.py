0b41ef297b44eb7178a5c678354409e14b316875
Matthew Sloss <msloss@calpoly.edu>
2022-08-17 19:50:20 -0700

    Converted temporarily to test for methods

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ import load_maker
 
 def main():
     aisc_df = pd.read_excel('__aisc-database-v15__.xlsx', sheet_name='Database v15.0')
-    while True:
-        try:
-            calculation = str(input("Tension/Compression/Flexural/Combined?"))
-            if calculation == 'Tension' or calculation == 'tension':
-                tension()
-            else: raise ValueError
-            break
-        except ValueError:
-            print("Invalid or Unsupported input")
+    member_df = member_maker.MemberMaker(aisc_df, 'W12X279', 10)
+    member_df.build_connection()
+
+    # while True:
+    #     try:
+    #         calculation = str(input("Tension/Compression/Flexural/Combined?"))
+    #         if calculation == 'Tension' or calculation == 'tension':
+    #             tension()
+    #         else: raise ValueError
+    #         break
+    #     except ValueError:
+    #         print("Invalid or Unsupported input")
+
+
     # member_df = member_maker.MemberMaker(aisc_df, 'W12X279', 10)
     # calc_tension = steel_solver.TensionMember(member_df, 25)
     # print(calc_tension.check_slenderness())
